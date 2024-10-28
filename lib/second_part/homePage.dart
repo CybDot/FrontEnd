3a2bb12f8275eadf76e51c1d87674f11d549e263
Rishab <rishabthapa7777@gmail.com>
2024-10-28 19:27:38 +0545

    making home page prototype

--- lib/second_part/homePage.dart
+++ lib/second_part/homePage.dart
@@ -1,2 +1,21 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_constructors
 
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String username;
+
+  HomePage({required this.username});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Center(
+        child: Text("Welcome, $username!", style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
