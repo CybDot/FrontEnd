b6860f2d45e2899a5d0d7c40f40bd2aa9081b8fe
Rishab <rishabthapa7777@gmail.com>
2024-10-28 20:22:21 +0545

    making homepage work able

--- lib/second_part/homePage.dart
+++ lib/second_part/homePage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_constructors
+// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_constructors, unused_element, prefer_typing_uninitialized_variables, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 
@@ -11,11 +11,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        automaticallyImplyLeading: false,
+        title: _appbar(username),
       ),
-      body: Center(
-        child: Text("Welcome, $username!", style: TextStyle(fontSize: 24)),
-      ),
+      body: Container(),
+    );
+  }
+
+  Widget _appbar(dynamic username) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text('Welcome, $username!'),
+      ],
     );
   }
 }
