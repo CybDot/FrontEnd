fc9e828900dbf43525637707fd64b0b85cf79d19
Rishab <rishabthapa7777@gmail.com>
2024-11-12 09:26:01 +0545

    trying svg image

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_constructors, unused_element, prefer_typing_uninitialized_variables, avoid_unnecessary_containers, library_private_types_in_public_api, avoid_print, non_constant_identifier_names
+// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_constructors, unused_element, prefer_typing_uninitialized_variables, avoid_unnecessary_containers, library_private_types_in_public_api, avoid_print, non_constant_identifier_names, deprecated_member_use
 //
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyProfile extends StatefulWidget {
   const MyProfile({super.key});
@@ -23,7 +24,20 @@ class _MyProfileState extends State<MyProfile> {
   AppBar ProfileAppbar() {
     return AppBar(
       title: Text('Profile'),
-      leading: Icon(Icons.menu),
+      leading: Container(
+        margin: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white70,
+        ),
+        alignment: Alignment.center,
+        child: SvgPicture.asset(
+          'assets/icons/leftArrow.svg',
+          height: 20,
+          width: 20,
+          color: Colors.black,
+        ),
+      ),
     );
   }
 }
